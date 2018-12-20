549febc50898dfdac0d68f6171a58f861299ec28
Marco Sangalli <msangalli@sapient.com>
2018-12-20 17:30:25 +0100

    adding initial tests

---
+++ src/lib/react-xstate-hoc.test.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react'
+import { withStateMachine } from './react-xstate-hoc';
+import { StateMachineInjectedProps } from './types';
+import { shallow } from 'enzyme';
+import { MachineConfig, EventObject } from 'xstate';
+
+// jest.mock('xstate', () => ({
+//     Machine: () => {
+//         initialState: {
+//             value: 'A'
+//         }
+//     }
+// }));
+
+interface HostedMachineStateSchema {
+    states: {
+        A: {};
+        B: {}
+    }
+}
+
+const machineMock: MachineConfig<{}, HostedMachineStateSchema, EventObject> = {
+    initial: 'A',
+    states: {
+        A: {},
+        B: {}
+    }
+});
+
+interface HostedComponentSchema {
+    states: {
+        A: {};
+        B: {};
+    }
+}
+
+type HostedComponentEvents =
+    | { type: 'DEFAULT' };
+
+
+interface HostedComponentProps extends StateMachineInjectedProps<{}, HostedComponentSchema, HostedComponentEvents> {
+}
+
+class HostedComponent extends React.Component<HostedComponentProps> {
+}
+
+
+describe('ReactXstateHoc', () => {
+    it('Should initilise interpreter when render called', () => {
+        const component = React.createElement(withStateMachine(HostedComponent, machineMock, {}));
+        const instance = shallow(component).instance() as any; // should be a type here
+        const spy = jest.spyOn(instance, 'initInterpreter');
+        instance.render();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('Should initilise interpreter when option passed', () => {
+        const component = React.createElement(withStateMachine(HostedComponent, machineMock, {}));
+        const instance = shallow(component).instance() as any; // should be a type here
+        const spy = jest.spyOn(instance, 'initInterpreter');
+        instance.setMachineOptions({});
+        expect(spy).toHaveBeenCalled();
+    });
+})
